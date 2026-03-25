1a0c3a315db4b73eab521e7d1e7f54d40c22439c
Maelle Rouvray <maelle.rouvray@gmail.com>
2026-03-25 23:13:15 +0100

    correction vraie version

--- game_of_life_domain_decomposition.py
+++ game_of_life_domain_decomposition.py
@@ -21,6 +21,9 @@ et inversement.
 
 On itère ensuite pour étudier la façon dont évolue la population des cellules sur la grille.
 
+Pour lancer le programme avec 4 processus : mpiexec -n 4 python3 game_of_life_domain_decomposition.py
+
+
 ################
 Parallélisation du code game_of_life.py
 
@@ -33,7 +36,6 @@ globCom = MPI.COMM_WORLD.Dup()
 nbp     = globCom.size
 rank    = globCom.rank
 name    = MPI.Get_processor_name()
-
 
 class Grille:
     """
@@ -66,22 +68,18 @@ class Grille:
         Rempli la grille avec le pattern donné en argument, en tenant compte du rang du processus pour ne remplir que la partie de la grille qui lui est attribuée
         """
         start_row = self.dimensions_local[0]*rank
-        end_row   = self.dimensions_local[0]*(rank+1) # chaque processus prend une seule ligne  
+        end_row   = self.dimensions_local[0]*(rank+1) # Chaque processus prend une seule ligne  
 
         for (i,j) in init_pattern:
             if start_row <= i < end_row :
                 self.cells[i -start_row + 1, j] = 1  
-
-
-
 
     def compute_next_iteration(self):
         """
         Calcule la prochaine génération de cellules en suivant les règles du jeu de la vie
         """
         # Remarque 1: on pourrait optimiser en faisant du vectoriel, mais pour plus de clarté, on utilise les boucles
-        # Remarque 2: on voit la grille plus comme une matrice qu'une grille géométrique. L'indice (0,0) est donc en bas
-        #             à gauche de la grille !
+        # Remarque 2: on voit la grille plus comme une matrice qu'une grille géométrique. L'indice (0,0) est donc en bas à gauche de la grille !
         ny = self.dimensions_local[0]
         nx = self.dimensions_global[1] # dimensions_global[1]=dimensions_local[1] chaque ligne reste de la même longueur pour tous les processus
         #next_cells = np.empty(self.dimensions_local, dtype=np.uint8)
@@ -104,14 +102,12 @@ class Grille:
                     else:
                         next_cells[i,j] = 1 # Sinon elle reste vivante
                 elif nb_voisines_vivantes == 3: # Cas où cellule morte mais entourée exactement de trois vivantes
-                    next_cells[i,j] = 1         # Naissance de la cellule
+                    next_cells[i,j] = 1 # Naissance de la cellule
                     diff_cells.append(i*nx+j)
                 else:
-                    next_cells[i,j] = 0         # Morte, elle reste morte.
-        self.cells[1:ny+1, :] = next_cells[1:ny+1, :]  # on remplace juste les lignes réelles et pas les ghosts cells
+                    next_cells[i,j] = 0 # Morte, elle reste morte.
+        self.cells[1:ny+1, :] = next_cells[1:ny+1, :]  # On remplace juste les lignes réelles et pas les ghosts cells
         return diff_cells
-        #return None
-
 
     def sync_ghosts_cells(self):
         """
@@ -125,23 +121,18 @@ class Grille:
             globCom.Send(self.cells[1,:], dest=voisin_haut) # On envoie la première ligne de la partie centrale de la grille (sans les ghost cells) au voisin du haut
             globCom.Recv(self.cells[self.dimensions_local[0]+1,:], source=voisin_bas) # On reçoit la ligne de bordure du voisin du bas et on la stocke dans la ghost cell du bas (la dernière ligne de self.cells)
 
-        else : # processus impairs
+        else : # Processus impairs
             globCom.Recv(self.cells[self.dimensions_local[0]+1,:], source=voisin_bas)
             globCom.Send(self.cells[1,:], dest=voisin_haut)
-
 
         # Vers le bas
         if rank%2 == 0 : # Processus pairs, on fait d'abord les échanges dans un sens, puis dans l'autre pour éviter les interblocages
             globCom.Send(self.cells[self.dimensions_local[0],:], dest=voisin_bas) # On envoie la dernière ligne de la partie centrale de la grille (sans les ghost cells) au voisin du bas
             globCom.Recv(self.cells[0,:], source=voisin_haut) # On reçoit la ligne de bordure du voisin du haut et on la stocke dans la ghost cell du haut (la première ligne de self.cells)   
 
-        else : # processus impairs
+        else : # Processus impairs
             globCom.Recv(self.cells[0,:], source=voisin_haut)
             globCom.Send(self.cells[self.dimensions_local[0],:], dest=voisin_bas)
-
-
-
-
 
 class App:
     """
@@ -163,7 +154,6 @@ class App:
         self.height= grid.dimensions_global[0] * self.size_y
         # Création de la fenêtre à l'aide de tkinter
         self.screen = pg.display.set_mode((self.width,self.height))
-        #
         self.canvas_cells = []
 
     def compute_rectangle(self, i: int, j: int):
@@ -178,7 +168,6 @@ class App:
         else:
             return self.grid.col_life
 
-
     def draw(self, global_cells):
         for i in range(self.grid.dimensions_global[0]):
             for j in range(self.grid.dimensions_global[1]):
@@ -191,13 +180,10 @@ class App:
                 pg.draw.line(self.screen, self.draw_color, (j*self.size_x, 0), (j*self.size_x, self.height))
         pg.display.update()
 
-
-
 if __name__ == '__main__':
     import time
     import sys
 
-    pg.init()
     dico_patterns = { # Dimension et pattern dans un tuple
         'blinker' : ((5,5),[(2,1),(2,2),(2,3)]),
         'toad'    : ((6,6),[(2,2),(2,3),(2,4),(3,3),(3,4),(3,5)]),
@@ -222,8 +208,9 @@ if __name__ == '__main__':
     if len(sys.argv) > 3 :
         resx = int(sys.argv[2])
         resy = int(sys.argv[3])
-    print(f"Pattern initial choisi : {choice}")
-    print(f"resolution ecran : {resx,resy}")
+    if rank == 0:
+        print(f"\nPattern initial choisi : {choice}")
+        print(f"resolution ecran : {resx,resy}\n")
     try:
         init_pattern = dico_patterns[choice]
     except KeyError:
@@ -244,42 +231,49 @@ if __name__ == '__main__':
         # On filtre le pattern pour éviter d'avoir des coordonnées hors grille
         init_coord = [(i, j) for (i, j) in init_coord if i < dim_global[0]]
 
-
     grid = Grille(dim=dim_global, init_pattern=init_coord)
-    
 
     if rank == 0 :
         pg.init()
         appli = App((resx, resy), grid)
     
     mustContinue = True
+    N_ITER_BENCHMARK = 200
+    iter_count = 0
     while mustContinue:
-        #time.sleep(0.5) # A régler ou commenter pour vitesse maxi
+        iter_count += 1
+        if iter_count >= N_ITER_BENCHMARK:
+            break
         t1 = time.time()
-        grid.sync_ghosts_cells() # synchronisation des ghost cells avant de calculer la prochaine génération
-        grid.compute_next_iteration() # calcul de la prochaine génération
+        grid.sync_ghosts_cells() # Synchronisation des ghost cells avant de calculer la prochaine génération
+        grid.compute_next_iteration() # Calcul de la prochaine génération
 
-        # reconstruction grille globale
-        local_block = grid.cells[1:grid.dimensions_local[0]+1,:] # partie centrale de la grille sans les ghost cells
-        local_flat = local_block.flatten() # on aplatit le bloc local pour pouvoir l'envoyer avec globCom
+        # Reconstruction grille globale
+        local_block = grid.cells[1:grid.dimensions_local[0]+1,:] # Partie centrale de la grille sans les ghost cells
+        local_flat = local_block.flatten() # On aplatit le bloc local pour pouvoir l'envoyer avec globCom
 
         recvbuff = None
         if rank == 0:
             recvbuff = np.empty(dim_global[0] * dim_global[1], dtype=np.uint8)
 
-        globCom.Gather(local_flat, recvbuff, root=0) # rassemblement de tous les blocs locaux dans recvbuff sur le processus 0
+        globCom.Gather(local_flat, recvbuff, root=0) # Rassemblement de tous les blocs locaux dans recvbuff sur le processus 0
 
         if rank == 0:
             global_grid = recvbuff.reshape(dim_global)
+
+            t_disp_start = time.time()
             appli.draw(global_grid)
-            t2 = time.time()
+            t_disp = time.time() - t_disp_start
+            t_total = time.time() - t1
+            print(f"[Rank 0] Total iter : {t_total:2.2e}s | Affichage : {t_disp:2.2e}s")
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     mustContinue = False
-
-            print(f"Duree d'une iteration : {t2-t1:2.2e} secondes")
+        
+        #mustContinue = globCom.bcast(mustContinue, root=0) # communication globale de l'arrêt (fermeture fenêtre d'affichage)
     pg.quit()
+
 
 
 
